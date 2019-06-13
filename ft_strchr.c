aafa632ce54a7c8edca650383e63ebdb825891fa
Lebogang SELAO <lselao@c4r12s3.wethinkcode.co.za>
2019-06-13 18:24:58 +0200

    updated_functions

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: lselao <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 16:30:00 by lselao            #+#    #+#             */
-/*   Updated: 2019/06/06 15:22:01 by lselao           ###   ########.fr       */
+/*   Updated: 2019/06/13 17:06:47 by lselao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strchr(const char *s, int c)
 	size_t i;
 
 	i = 0;
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return ((char *)s + i);
 		i++;
 	}
-	if (!c && s[i] == '\0')
-		return ((char *)s + 1);
+	if (c == '\0')
+		return ((char *)s + i);
 	return (NULL);
 }
