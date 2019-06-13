7d36513feecc867235e1406457f3a379c1d8424f
Lebogang SELAO <lselao@c4r12s3.wethinkcode.co.za>
2019-06-13 18:48:45 +0200

    updated_functions

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: lselao <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 16:42:14 by lselao            #+#    #+#             */
-/*   Updated: 2019/06/12 10:40:35 by lselao           ###   ########.fr       */
+/*   Updated: 2019/06/13 18:46:53 by lselao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,13 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	i = 0;
 	chr = (unsigned char)c;
 	source = (unsigned char *)src;
-	dest = (unsigned char *)dst;
-	if (dst && src && c && n > 0)
+	dest = dst;
+	while (i < n)
 	{
-		while (i < n)
-		{
-			dest[i] = source[i];
-			if (source[i] == chr)
-			{
-				return (dst + i + 1);
-			}
-			i++;
-		}
+		dest[i] = source[i];
+		if (source[i] == chr)
+			return (dst + i + 1);
+		i++;
 	}
 	return (NULL);
 }
